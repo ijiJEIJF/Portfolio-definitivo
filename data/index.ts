7dc2572bf9ea485b66d47285474986ba5c2f5ddf
ijiJEIJF <98031059+ijiJEIJF@users.noreply.github.com>
2024-11-21 15:46:32 +0100

    first commit

---
+++ data/index.ts
@@ -0,0 +1,167 @@
+export const navItems = [
+  { name: "Curiosità", link: "#about" },
+  { name: "Progetti", link: "#projects" },
+  { name: "Recensioni", link: "#testimonials" },
+  { name: "Contattami", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Do priorità alla cominicazione, capendo ogni problema",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Da qualsiasi parte del mondo!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Qualsiasi cosa",
+    description: "Continuo sempre ad imparare",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Passione per il Development e il Marketing",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "TiagoReplicass Site",
+    description: "Per cosa sto lavorando in questo momento?",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "F1r3 x TiagoReplicass Site",
+    des: "Un sito realizzato su misura per Tiago Replicass.",
+    img: "/TiagoReplicass.PNG",
+    iconLists: ["/html.PNG", "/css.svg"],
+    link: "/https://tiagoxfinds.vercel.app/",
+  },
+    {
+    id: 1,
+    title: "NexMC",
+    des: "Un server Minecraft con tante modalità innovative.",
+    img: "/NexLogo.PNG",
+    iconLists: ["/spigot.PNG", "/mc.svg"],
+    link: "/https://tiagoxfinds.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "???",
+    name: "???",
+    title: "???",
+  },
+  {
+    quote:
+      "???",
+    name: "???",
+    title: "???",
+  },
+  {
+    quote:
+      "???",
+    name: "???",
+    title: "???",
+  },
+  {
+    quote:
+      "???",
+    name: "???",
+    title: "???",
+  },
+  {
+    quote:
+      "",
+    name: "???",
+    title: "???",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "",
+    img: "",
+    nameImg: "",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Minecraft Plugginner",
+    desc: "Ho molta esperienza con la maggior parte dei plugin di Minecraft in circolazione al momento.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Texture Pack & Custom Items Modeler",
+    desc: "Creo Texture Pack personalizzare e anche Item e boss custom per Minecraft.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Marketing",
+    desc: "Posso anche aiutare a far crescere il tuo server con tecniche di marketing.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Discord and Web Dev",
+    desc: "Creo Bot discord e siti web.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
